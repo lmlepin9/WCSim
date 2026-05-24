50caddfd55fb532470cfbf5f50230e0afdecad1b
lmlepin9 <lml24d@fsu.edu>
2026-05-23 21:40:09 -0500

    Adding PMT tilt and shift

--- src/WCSimConstructANNIECylinderScan.cc
+++ src/WCSimConstructANNIECylinderScan.cc
@@ -232,19 +232,26 @@ G4LogicalVolume* WCSimDetectorConstruction::ConstructANNIECylinderScan()
 	
 	// Create rotation matrices for the orientations of the PMTs
 	std::vector<G4RotationMatrix*> pmt_rotation_matrices;
+	std::vector<G4RotationMatrix*> tilted_pmt_rotation_matrices;
 	// Bottom PMTs have panel number 0
 	G4RotationMatrix *WCBottomCapRotation = new G4RotationMatrix();
-	pmt_rotation_matrices.push_back(WCBottomCapRotation);	
+	pmt_rotation_matrices.push_back(WCBottomCapRotation);
+	tilted_pmt_rotation_matrices.push_back(WCBottomCapRotation);
 	// Barrel PMTs have panel numbers 1-8
 	for(int facei=0; facei<WCBarrelRingNPhi; facei++){
 		G4RotationMatrix* WCPMTRotationNext = new G4RotationMatrix(*WCPMTRotation);
+		G4RotationMatrix* WCPMTtiltRotationNext = new G4RotationMatrix(*WCPMTRotation);
 		WCPMTRotationNext->rotateX((dPhi*facei)-67.5*deg+180*deg);
+		WCPMTtiltRotationNext->rotateX((dPhi*facei)-67.5*deg+180*deg);
+		WCPMTtiltRotationNext->rotateY(-53.*deg);
 		pmt_rotation_matrices.push_back(WCPMTRotationNext);
+		tilted_pmt_rotation_matrices.push_back(WCPMTtiltRotationNext);
 	}
 	// Top PMTs have panel number 9
 	G4RotationMatrix *WCTopCapRotation = new G4RotationMatrix();
 	WCTopCapRotation->rotateY(180.*deg);
 	pmt_rotation_matrices.push_back(WCTopCapRotation);
+	tilted_pmt_rotation_matrices.push_back(WCTopCapRotation);
 
 	G4cout <<"Size of pmt_rotation_matrices: "<<pmt_rotation_matrices.size()<<G4endl;
 
@@ -260,10 +267,12 @@ G4LogicalVolume* WCSimDetectorConstruction::ConstructANNIECylinderScan()
 		if (pmt_position_file.eof()) break;
 		//G4cout << "Read in PMT "<<PMTID<<", panel nr: "<<panel_nr<<", Position ("<<pmt_x<<","<<pmt_y<<","<<pmt_z<<"), PMT type: "<<pmt_type<<G4endl;
 		G4LogicalVolume *logicWCPMT = logicWCPMTs.at(pmt_type);
-		G4RotationMatrix *pmt_rot = pmt_rotation_matrices.at(panel_nr);
+		G4bool useTiltedPMT = ((pmt_type == 3) || (pmt_type == 0 && panel_nr != 0));
+		G4RotationMatrix *pmt_rot = useTiltedPMT ? tilted_pmt_rotation_matrices.at(panel_nr)
+		                                        : pmt_rotation_matrices.at(panel_nr);
 		pmt_x_shift = pmt_x*cm;
 		pmt_y_shift = (168.1-pmt_z)*cm;
-		pmt_z_shift = ((pmt_y+14.45))*cm;
+		pmt_z_shift = (pmt_y+14.45+(useTiltedPMT ? 13.9 : 0.))*cm;
 		//pmt_z_shift = ((pmt_y+14.45)-InnerStructureCentreOffset/10.)*cm;
 		//G4cout <<"Edited PMT position ("<<pmt_x_shift<<","<<pmt_y_shift<<","<<pmt_z_shift<<")"<<G4endl;
 		G4ThreeVector PMTPosition(pmt_x_shift,pmt_y_shift,pmt_z_shift);
